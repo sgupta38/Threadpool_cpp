695ef019e9763a7e4f031875df5d692d89b6a5ca
Sonu Gupta <sgupta38@binghamton.edu>
2020-04-29 19:57:44 -0400

    Added typedef and restricted some functions

--- threadpool.cpp
+++ threadpool.cpp
@@ -1,4 +1,55 @@
-//
-// Created by sonug on 4/29/2020.
-//
+#include "threadpool.h"
 
+void threadpool::initialize_pool(int count)
+{
+    for(int i= 0; i< thread_cnt; ++i)
+        pool.emplace_back(&threadpool::dispatch_handler, this);
+}
+
+void threadpool::add_to_queue(const fp_t& f)
+{
+    std::unique_lock<std::mutex> lock_(m);
+    dispatch_queue.push(f);
+    lock_.unlock(); // unlock will be done anyway by end of function. But reducing latency.
+    cv.notify_all();
+}
+
+void threadpool::add_to_queue(fp_t&& f)
+{
+    std::unique_lock<std::mutex> lock_(m);
+    dispatch_queue.push(std::move(f));
+    lock_.unlock(); // unlock will be done anyway by end of function. But reducing latency.
+    cv.notify_all();
+}
+
+void threadpool::dispatch_handler()
+{
+    std::unique_lock<std::mutex> lock_(m);
+    do{
+        cv.wait(lock_, [this] { return (dispatch_queue.size() || isQuit); });
+        if(!isQuit && dispatch_queue.size())
+        {
+            auto task = dispatch_queue.front();
+            dispatch_queue.pop();
+            lock_.unlock(); // unlock since we are done using shared resource (queue)
+
+            task(); // execute function
+
+            lock_.lock();
+        }
+    }while(!isQuit);
+}
+
+threadpool::~threadpool()
+{
+    std::unique_lock<std::mutex> lock_(m);
+    isQuit = true;
+    lock_.unlock();
+    cv.notify_all();
+
+    for(int i = 0; i < pool.size(); ++i)
+    {
+        if(pool[i].joinable())
+            pool[i].join();
+    }
+}
--- threadpool.h
+++ threadpool.h
@@ -16,24 +16,33 @@ using namespace std;
 
 class threadpool
 {
+    typedef std::function<void(void)> fp_t;
 private:
     bool isQuit;
     std::mutex m;
     int thread_cnt;
     std::vector<std::thread> pool;
     std::condition_variable cv;
-    std::queue<std::function<void()>> dispatch_queue;
+    std::queue<fp_t> dispatch_queue;
 
 public:
+
     threadpool(int count):thread_cnt{count}, isQuit{false}
     {
         initialize_pool(count);
     }
 
     void initialize_pool(int count);
-    void add_to_queue(std::function<void()> f);
+    void add_to_queue(const fp_t& f); // dispatch and copy
+    void add_to_queue(fp_t&& f);      // dispatch and move
     void dispatch_handler();
     ~threadpool();
+
+    //deleted functions
+    threadpool(const threadpool& rhs) = delete;
+    threadpool(threadpool&& rhs) = delete;
+    threadpool& operator=(const threadpool& rhs) = delete;
+    threadpool& operator=(threadpool&& rhs) = delete;
 };
 
 #endif //THREADPOOL_THREADPOOL_H
